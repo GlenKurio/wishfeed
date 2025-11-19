654c308aa53b6303e2d9f205a36b66020c353a7d
Oleh Minko <gleb.kurip@gmail.com>
2025-11-19 13:11:49 -0600

    initial commit with styles setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <>
+      <p>Ola amigo</p>
+      <button className="btn btn-primary">
+        SHould look like a daisy UI button
+      </button>
+    </>
+  );
+}
+
+export default App;
