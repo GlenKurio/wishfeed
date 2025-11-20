1f9074fdd40684ceffa9954348470cacd836a7d0
Oleh Minko <gleb.kurip@gmail.com>
2025-11-20 00:12:27 -0600

    daisy ui setup with custom theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 function App() {
   return (
-    <>
-      <p>Ola amigo</p>
-      <button className="btn btn-primary">
+    <div className="bg-base-100 h-screen">
+      <p className="text-5xl text-primary">Ola amigo</p>
+      <button className="btn btn-primary ">
         SHould look like a daisy UI button
       </button>
-    </>
+    </div>
   );
 }
 
