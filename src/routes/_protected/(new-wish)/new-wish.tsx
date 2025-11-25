bac264b65e7b8a5b792d7b8849f7233d747b3d3a
Oleh Minko <gleb.kurip@gmail.com>
2025-11-25 13:19:27 -0600

    todo update

--- src/routes/_protected/(new-wish)/new-wish.tsx
+++ src/routes/_protected/(new-wish)/new-wish.tsx
@@ -1,9 +1,13 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/_protected/(new-wish)/new-wish')({
+export const Route = createFileRoute("/_protected/(new-wish)/new-wish")({
   component: RouteComponent,
-})
-
+});
+// TODO:
+// 3 steps process:
+// 1. Paste the link input or fill the form below; Click next
+// 2. Preview, if link was pasted - show the loading ui while scraping, and fill out the preview with ability to edit. On edit open the page/overlay with form-like fields to allow edit, and save to go backl to preview; Preview has button "Publish" or wish for it! On wish for it we are publishing the wish and swapping the url to our affiliate link;
+// 3. Success with return to home page?
 function RouteComponent() {
-  return <div>Hello "/(new-wish)/new-wish"!</div>
+  return <div>Hello "/(new-wish)/new-wish"!</div>;
 }
