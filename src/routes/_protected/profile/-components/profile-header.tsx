cefdb96a2695a864ed9e5458c3317c94050fd798
Oleh Minko <gleb.kurip@gmail.com>
2025-12-10 10:57:33 -0600

    profile header redesign to fit birthday and bio

--- src/routes/_protected/profile/-components/profile-header.tsx
+++ src/routes/_protected/profile/-components/profile-header.tsx
@@ -3,7 +3,8 @@ import { Link } from "@tanstack/react-router";
 import Avatar from "@/components/avatar";
 import { useFollowUser } from "@/hooks/use-follow-user";
 import type { UserProfile } from "@/lib/types";
-
+import { IconCake } from "@tabler/icons-react";
+import { format, parseISO } from "date-fns";
 export default function ProfileHeader({
   userProfile,
   isOwner,
@@ -14,91 +15,30 @@ export default function ProfileHeader({
   const { isFollowing } = useFollowUser({ userId: userProfile?.uid });
 
   return (
-    <>
-      <div className="bg-background w-full">
-        <div className="mx-auto">
-          {/* Main profile section */}
-          <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:justify-between">
-            {/* Left side: Avatar + Info + Stats */}
-            <div className="flex flex-col gap-6 lg:flex-row lg:items-center lg:gap-8">
-              <div className="flex items-center gap-4">
-                <Avatar src={userProfile?.photoURL} className="w-14 md:w-20" />
-                <div className="shrink-0">
-                  <h1 className="text-foreground text-2xl leading-tight font-bold">
-                    {userProfile?.displayName}
-                  </h1>
-                  <p className="text-muted-foreground text-base md:text-lg">
-                    @{userProfile?.handle}
-                  </p>
-                </div>
-              </div>
-              <div className="flex flex-col gap-4 px-2">
-                <div className="flex w-full items-center gap-6 md:gap-8">
-                  <Link
-                    to="/profile/$userId/followers"
-                    params={{
-                      userId: userProfile?.uid,
-                    }}
-                    className="group transition-colors hover:opacity-80"
-                  >
-                    <div>
-                      <p className="text-foreground text-lg font-bold md:text-xl">
-                        {userProfile?.followers.length || 0}
-                      </p>
-                      <p className="text-muted-foreground group-hover:text-foreground text-sm">
-                        Followers
-                      </p>
-                    </div>
-                  </Link>
+    <div className="w-full">
+      <div className="flex items-start gap-2 md:gap-4">
+        {/* Avatar */}
+        <Avatar src={userProfile?.photoURL} className="w-16 md:w-24" />
+        {/* Proifle info: title, handle, actions, bio */}
+        <div className="flex flex-col">
+          {/* Profile name with handle and profile actions */}
 
-                  <div className="bg-neutral/10 h-10 w-px" />
-
-                  <Link
-                    to="/profile/$userId/following"
-                    params={{
-                      userId: userProfile?.uid,
-                    }}
-                  >
-                    <p className="text-foreground text-lg font-bold md:text-xl">
-                      {userProfile?.following.length || 0}
-                    </p>
-                    <p className="text-muted-foreground text-sm">Following</p>
-                  </Link>
-
-                  <div className="bg-neutral/10 h-10 w-px" />
-
-                  <Link
-                    to="/profile/$userId/$wishlist/feed"
-                    params={{
-                      userId: userProfile?.uid,
-                      wishlist: "all",
-                    }}
-                    disabled={
-                      !userProfile.postsCount || userProfile.postsCount === 0
-                    }
-                  >
-                    <p className="text-foreground text-lg font-bold md:text-xl">
-                      {userProfile?.postsCount || 0}
-                    </p>
-                    <p className="text-muted-foreground text-sm">
-                      {userProfile?.postsCount === 1 ? "Post" : "Posts"}
-                    </p>
-                  </Link>
-                </div>
-              </div>
-            </div>
-
+          {/* Profile title */}
+          <div className="flex items-center gap-4">
+            <h1 className="text-foreground leading-tight font-bold md:text-lg lg:text-2xl">
+              {userProfile?.displayName}
+            </h1>
             <div className="">
               {isOwner ? (
                 <>
                   <Link
                     to="/profile/$userId/edit-profile"
                     params={{ userId: userProfile?.uid }}
-                    className="btn btn-sm md:btn-md w-full md:w-auto"
+                    className="btn btn-xs md:btn-sm w-full md:w-auto"
                   >
                     Edit Profile
                   </Link>
-                  <Link
+                  {/* <Link
                     to="/profile/$userId/$wishlist"
                     params={{
                       userId: "JyLsRANwzbSZukKtZ0WqYjY1moh2",
@@ -107,7 +47,7 @@ export default function ProfileHeader({
                     className="btn btn-sm md:btn-md flex-1 sm:flex-none"
                   >
                     Go to another User
-                  </Link>
+                  </Link> */}
                 </>
               ) : isFollowing ? (
                 <button className="btn w-full sm:w-auto">Unfollow</button>
@@ -116,8 +56,90 @@ export default function ProfileHeader({
               )}
             </div>
           </div>
+          <p className="text-muted-foreground mb-1 text-xs md:text-sm lg:text-base">
+            @{userProfile?.handle}
+          </p>
+          <div className="flex w-full items-center gap-2 md:gap-6">
+            <Link
+              to="/profile/$userId/followers"
+              params={{
+                userId: userProfile?.uid,
+              }}
+              className="flex items-center gap-1 md:gap-2"
+            >
+              <p className="text-foreground text-sm font-bold md:text-xl">
+                {userProfile?.followers.length || 0}
+              </p>
+              <p className="text-muted-foreground group-hover:text-foreground text-xs md:text-sm">
+                Followers
+              </p>
+            </Link>
+
+            <div className="bg-neutral/10 h-4 w-px md:h-8" />
+
+            <Link
+              to="/profile/$userId/following"
+              params={{
+                userId: userProfile?.uid,
+              }}
+              className="flex items-center gap-1 md:gap-2"
+            >
+              <p className="text-foreground text-sm font-bold md:text-xl">
+                {userProfile?.following.length || 0}
+              </p>
+              <p className="text-muted-foreground group-hover:text-foreground text-xs md:text-sm">
+                Following
+              </p>
+            </Link>
+
+            <div className="bg-neutral/10 h-4 w-px md:h-8" />
+
+            <Link
+              to="/profile/$userId/$wishlist/feed"
+              params={{
+                userId: userProfile?.uid,
+                wishlist: "all",
+              }}
+              disabled={!userProfile.postsCount || userProfile.postsCount === 0}
+              className="flex items-center gap-1 md:gap-2"
+            >
+              <p className="text-foreground text-sm font-bold md:text-xl">
+                {userProfile?.postsCount || 0}
+              </p>
+              <p className="text-muted-foreground group-hover:text-foreground text-xs md:text-sm">
+                {userProfile?.postsCount === 1 ? "Post" : "Posts"}
+              </p>
+            </Link>
+          </div>
+          <ProfileBirthday birthday={userProfile.birthday} />
+
+          {/* Bio */}
+          <p className="text-base-content/60 mt-2 max-w-md text-xs md:text-sm">
+            {" "}
+            {userProfile.bio}
+          </p>
         </div>
       </div>
+    </div>
+  );
+}
+
+function ProfileBirthday({ birthday }: { birthday?: string | null }) {
+  let formattedBirthday: string | null = null;
+
+  if (!birthday || birthday === "") return null;
+
+  const date = parseISO(birthday);
+  formattedBirthday = format(date, "MMMM do");
+
+  return (
+    <>
+      {formattedBirthday && (
+        <div className="mt-1 flex items-center gap-1">
+          <IconCake className="size-4 text-rose-300" />
+          <span className="text-xs leading-0">{formattedBirthday}</span>
+        </div>
+      )}
     </>
   );
 }
